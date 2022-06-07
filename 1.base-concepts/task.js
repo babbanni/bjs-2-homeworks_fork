11bf12f70b7269515e546bdedcb51b86c654d678
babbanni <vlad_paltsev@bk.ru>
2022-06-07 23:22:25 +0300

    solveEquation Edited

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,6 +3,18 @@
 function solveEquation(a, b, c) {
     let arr;
     // код для задачи №1 писать здесь
+    let x;
+    let d = b ** 2 - 4 * a * c;
+    if (d < 0) {
+        arr = [];
+    } else if (d === 0) {
+        x = -b / (2 * a);
+        arr = [x];
+    } else if (d > 0) {
+        x = (-b + Math.sqrt(d)) / (2 * a) && (-b - Math.sqrt(d)) / (2 * a);
+        arr = [x];
+    }
+
     return arr; // array
 }
 
