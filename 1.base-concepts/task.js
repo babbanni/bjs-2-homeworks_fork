f401ca144dfa9e40f26b0c2c431abb03862b0068
babbanni <vlad_paltsev@bk.ru>
2022-06-08 16:45:06 +0300

    task.js edited

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,22 +1,23 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-    let arr;
+    let arr = [];
     // код для задачи №1 писать здесь
     let x;
+    let y;
     let d = b ** 2 - 4 * a * c;
-    if (d < 0) {
-        arr = [];
-    } else if (d === 0) {
+    if (d === 0) {
         x = -b / (2 * a);
         arr = [x];
     } else if (d > 0) {
-        x = (-b + Math.sqrt(d)) / (2 * a) && (-b - Math.sqrt(d)) / (2 * a);
-        arr = [x];
+        x = (-b + Math.sqrt(d)) / (2 * a);
+        y = (-b - Math.sqrt(d)) / (2 * a);
+        arr = [Number(x.toFixed(2)), Number(y.toFixed(2))];
     }
 
     return arr; // array
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
     let totalAmount;
@@ -26,8 +27,8 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     let months = (date.getFullYear() - startDate.getFullYear()) * 12;
     months -= startDate.getMonth();
     months += date.getMonth();
-    let Payment = body * (p + (p / (((1 + p) ** months) - 1)));
-    totalAmount = months * Payment;
+    let payment = body * (p + (p / (((1 + p) ** months) - 1)));
+    totalAmount = months * payment;
     // код для задачи №2 писать здесь
-    return totalAmount.toFixed(2);
+    return Number(totalAmount.toFixed(2));
 }
