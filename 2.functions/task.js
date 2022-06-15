0db127639ccf7ffd9c5f987a8d25e73f87d91443
babbanni <vlad_paltsev@bk.ru>
2022-06-15 14:27:10 +0300

    getArrayParams edited

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,42 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+    let min, max, sum, avg;
+    min = Math.min(...arr);
+    max = Math.max(...arr);
+    sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+        avg = sum / arr.length;
+        if (arr[i] > max) {
+            max === arr[i];
+        } else if (arr[i] < min) {
+            min === arr[i];
+        }
+    }
+    // Ваш код
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+    return { min: min, max: max, avg: Number(avg.toFixed(2)) };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+    let sum;
 
-  // Ваш код
+    // Ваш код
 
-  return sum;
+    return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+    let max;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+    // Ваш кода
+    // for ...
+
+    return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    // Ваш код
 }
