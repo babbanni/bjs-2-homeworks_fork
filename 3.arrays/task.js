373ff65d7e7a5a716f32b3b5c4cb14040d9c2496
babbanni <vlad_paltsev@bk.ru>
2022-06-19 18:26:18 +0300

    task.js edited

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+    let result = false;
+    if (arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i])) {
+        result = true;
+    }
+    return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+    let resultArr = arr.filter(number => number >= 0).filter(number => number % 3 === 0).map(number => number * 10);
 
-  // Ваш код
 
-  return resultArr; // array
+
+    return resultArr; // array
 }
