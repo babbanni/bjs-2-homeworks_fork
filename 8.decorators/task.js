379cf31273023173a67a690f6f2603ad9b1793e7
babbanni <vlad_paltsev@bk.ru>
2022-07-17 19:46:26 +0300

    task.js edited

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,53 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = args.join(',');
+        let objectInCache = cache.find((item) => item.hash === hash);
+        if (!!objectInCache) {
+            return `Из кэша: ${objectInCache.result}`;
+        }
+
+        let result = func(...args);
+        cache.push({ hash: `${hash}`, result: `${result}` });
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        return `Вычисляем: ${result}`;
+    }
+    return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+    let flag = false;
+    return function() {
+        if (flag === true) {
+            console.log('проигнорировано, флаг поднят');
+            return;
+        }
+        func();
+        flag = true;
+        setTimeout(() => flag = false, delay)
+    }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+const sendSignal = () => console.log("Сигнал отправлен");
+const upgradedSendSignal = debounceDecorator2(sendSignal, 5000);
+
+function debounceDecorator2(func, delay) {
+    let flag = false;
+    let count = 0;
+    return function wrapper() {
+        if (flag === true) {
+            console.log('проигнорировано, флаг поднят');
+            return;
+        }
+        func();
+        count += 1;
+        wrapper.count = count;
+        flag = true;
+        setTimeout(() => flag = false, delay);
+        console.log(`Сигналов отправлено всего: ${wrapper.count}`);
+    }
 }
